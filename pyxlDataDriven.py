c765a8c9501516ec594ffa59a8a3acac27f01598
vinitha edwin <vinithaedwin@gmail.com>
2020-05-14 17:48:24 +0530

    datadriven pyxl

---
+++ pyxlDataDriven.py
@@ -0,0 +1,17 @@
+import openpyxl
+path ="D:\Vinitha\pyxl.xlsx"
+
+workbook = openpyxl.load_workbook(path)
+sheet = workbook.active
+
+rows= sheet.max_row
+cols=sheet.max_column
+
+print (rows)
+print(cols)
+
+for r in range(1,rows+1):
+    for c in range(1,cols+1):
+        print (sheet.cell(row=r,column=c).value, end="  ")
+    print()
+
